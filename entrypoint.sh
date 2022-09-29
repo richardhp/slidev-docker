44fa64ee1988c8232aa9edb40a382a5c914a9931
Richard Howell-Peak <development@richardhowellpeak.com>
2022-09-30 08:37:23 +1300

    it works

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-for FILE in `ls /tmp/slides | sort -g`; do echo "$FILE"; done
-pwd
-ls
-./node_modules/.bin/slidev --open
+SLIDES_DIR="/tmp/slides";
+rm "./slides.md";
+touch "./slides.md";
+for FILE in `ls $SLIDES_DIR | sort -g`; do 
+  cat "$SLIDES_DIR/$FILE" >> ./slides.md;
+  echo "" >> ./slides.md;
+  echo "---" >> ./slides.md;
+  echo "" >> ./slides.md;
+done
+
+cat ./slides.md;
+
+./node_modules/.bin/slidev build
---
+++ serve.Dockerfile
@@ -0,0 +1,2 @@
+FROM nginx:1.23.1-alpine
+
